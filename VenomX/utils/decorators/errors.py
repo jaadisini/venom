4077616867ee0a10bb81b98813a37e70fc5cb53e
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-02-15 13:49:53 +0700

    Update errors.py

--- VenomX/utils/decorators/errors.py
+++ VenomX/utils/decorators/errors.py
@@ -6,7 +6,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from wbb import LOG_GROUP_ID, app
+from config import LOGGER_ID, 
 
 
 def split_limits(text):

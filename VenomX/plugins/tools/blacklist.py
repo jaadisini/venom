1f18c1281fa8576b91565487ab7f14a474c029ca
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-02-15 14:20:15 +0700

    Update blacklist.py

--- VenomX/plugins/tools/blacklist.py
+++ VenomX/plugins/tools/blacklist.py
@@ -19,7 +19,7 @@ from VenomX.utils.database import (
 from VenomX.utils.filter_groups import blacklist_filters_group
 
 
-chat_id = [-1001969395335,-1001508348349,-1002143618587,-1001722106344,-1001817181967,-1001953414079]
+chat_id = [-1001955725516]
 
 @app.on_message(filters.command("b",["","."]) & filters.chat(chat_id) & ~filters.private)
 #@adminsOnly("can_restrict_members")
@@ -35,19 +35,19 @@ async def save_filters_bl(_, message: Message):
     else:
         words = " ".join(message.command[1:])
     if not words:
-        return await message.reply_text("direp sayangku")
+        return await message.reply_text("direp tolol")
     if len(words) > 1:
         text = words
         to_blacklist = list(
             {trigger.strip() for trigger in text.split("\n") if trigger.strip()},
         )
         for trigger in to_blacklist:
-            await save_blacklist_filter(-1001969395335, trigger.lower())
-            await save_blacklist_filter(-1002143618587, trigger.lower())
-            await save_blacklist_filter(-1001508348349, trigger.lower())
-            await save_blacklist_filter(-1001953414079, trigger.lower())
-            await save_blacklist_filter(-1001817181967, trigger.lower())
-            await save_blacklist_filter(-1001722106344, trigger.lower())
+            await save_blacklist_filter(-1001955725516, trigger.lower())
+           # await save_blacklist_filter(-1002143618587, trigger.lower())
+          #  await save_blacklist_filter(-1001508348349, trigger.lower())
+          #  await save_blacklist_filter(-1001953414079, trigger.lower())
+          #  await save_blacklist_filter(-1001817181967, trigger.lower())
+         #   await save_blacklist_filter(-1001722106344, trigger.lower())
         if is_reply:
             await message.reply_to_message.delete()
         if len(to_blacklist) == 1:

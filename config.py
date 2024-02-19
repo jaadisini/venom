efb8f6a1b7ae108b6ab54f35242e0021346ac08a
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-02-20 01:38:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
+    "GIT_TOKEN", "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"
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/stayheresay")

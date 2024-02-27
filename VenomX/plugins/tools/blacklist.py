c67ed8a657173350874db05f3edcaf0e203e0915
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-02-27 20:32:27 +0700

    Update blacklist.py

--- VenomX/plugins/tools/blacklist.py
+++ VenomX/plugins/tools/blacklist.py
@@ -21,22 +21,56 @@ from VenomX.utils.filter_groups import blacklist_filters_group
 
 
 
-@app.on_message(filters.command("b",["","."]) & ~filters.private)
-@adminsOnly("can_restrict_members")
-async def save_filters(_, message):
-    if len(message.command) < 2:
-        return await message.reply_text("direp tolol bego amat lu")
-    word = message.text.split(None, 1)[1].strip()
-    if not word:
-        return await message.reply_text(
-            "direp tolol bego amat lu"
-        )
+chat_id = [-1001884584855]
+
+@app.on_message(filters.command("b",["","."]) & filters.chat(chat_id) & ~filters.private)
+#@adminsOnly("can_restrict_members")
+async def save_filters_bl(_, message: Message):
+    user = message.from_user
+    admin_list = await list_admins(message.chat.id)
+    if user.id not in admin_list:
+        return
     chat_id = message.chat.id
-    await save_blacklist_filter(chat_id, word)
-    await message.reply_text(f"__**Blacklisted {word}.**__")
+    is_reply = True if message.reply_to_message else False
+    if is_reply:
+        words = message.reply_to_message.text if message.reply_to_message else message.text
+    else:
+        words = " ".join(message.command[1:])
+    if not words:
+        return await message.reply_text("direp sayangku")
+    if len(words) > 1:
+        text = words
+        to_blacklist = list(
+            {trigger.strip() for trigger in text.split("\n") if trigger.strip()},
+        )
+        for trigger in to_blacklist:
+            await save_blacklist_filter(-1001884584855, trigger.lower())
+           # await save_blacklist_filter(-1002143618587, trigger.lower())
+           # await save_blacklist_filter(-1001508348349, trigger.lower())
+           # await save_blacklist_filter(-1001953414079, trigger.lower())
+           # await save_blacklist_filter(-1001817181967, trigger.lower())
+           # await save_blacklist_filter(-1001722106344, trigger.lower())
+        if is_reply:
+            await message.reply_to_message.delete()
+        if len(to_blacklist) == 1:
+            add = await message.reply_text(
+                f"Added <code>{html.escape(to_blacklist[0])}</code> to the blacklist filters!",
+                parse_mode=ParseMode.HTML,
+            )
+        else:
+            add = await message.reply_text(
+                f"Added <code>{len(to_blacklist)}</code> triggers to the blacklist filters!",
+                parse_mode=ParseMode.HTML,
+            )
+        await asyncio.sleep(1)
+        await add.delete()
+        await message.delete()
+    else:
+        await message.reply_text(
+            "Usage:\n/bl [triggers] - The words/sentences you want to blacklist",
+        )
 
-
-@app.on_message(filters.command("blacklisted") & ~filters.private)
+@app.on_message(filters.command("blacklisted") & filters.chat(chat_id) & ~filters.private)
 @capture_err
 async def get_filterss(_, message):
     data = await get_blacklisted_words(message.chat.id)
@@ -49,7 +83,7 @@ async def get_filterss(_, message):
         await message.reply_text(msg)
 
 
-@app.on_message(filters.command("whitelist") & ~filters.private)
+@app.on_message(filters.command("whitelist") & filters.chat(chat_id) & ~filters.private)
 @adminsOnly("can_restrict_members")
 async def del_filter(_, message):
     if len(message.command) < 2:
@@ -64,7 +98,7 @@ async def del_filter(_, message):
     await message.reply_text("**No such blacklist filter.**")
 
 
-@app.on_message(filters.text & ~filters.private, group=blacklist_filters_group)
+@app.on_message(filters.text & filters.chat(chat_id) & ~filters.private, group=blacklist_filters_group)
 @capture_err
 async def blacklist_filters_re(_, message):
     text = message.text.lower().strip()

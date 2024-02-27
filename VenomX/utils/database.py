78a5250b4f39de385cda1c0de95582c3fbeddeec
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-02-27 12:48:49 +0700

    Update database.py

--- VenomX/utils/database.py
+++ VenomX/utils/database.py
@@ -25,6 +25,7 @@ sudoersdb = mongodb.sudoers
 usersdb = mongodb.tgusersdb
 queriesdb = mongodb.queries
 blacklist_filtersdb = mongodb.blacklistFilters
+restart_stagedb = db.restart_stage
 
 # Shifting to memory [mongo sucks often]
 active = []
@@ -740,3 +741,26 @@ async def save_blacklist_filter(chat_id: int, word: str):
         {"$set": {"filters": _filters}},
         upsert=True,
     )
+
+async def start_restart_stage(chat_id: int, message_id: int):
+    await restart_stagedb.update_one(
+        {"something": "something"},
+        {
+            "$set": {
+                "chat_id": chat_id,
+                "message_id": message_id,
+            }
+        },
+        upsert=True,
+    )
+
+
+async def clean_restart_stage() -> dict:
+    data = await restart_stagedb.find_one({"something": "something"})
+    if not data:
+        return {}
+    await restart_stagedb.delete_one({"something": "something"})
+    return {
+        "chat_id": data["chat_id"],
+        "message_id": data["message_id"],
+    }
